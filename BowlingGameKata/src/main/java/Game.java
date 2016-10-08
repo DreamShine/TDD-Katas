f80951de0f35b444ea7c0627cba466fb39faba10
DreamShine <wizard.jackalope@gmail.com>
2016-10-08 15:09:52 -0500

    More Tests and refactoring. Added collection for keeping track of the frame currently being rolled on as well as adding support for spares.

--- BowlingGameKata/src/main/java/Game.java
+++ BowlingGameKata/src/main/java/Game.java
@@ -3,15 +3,24 @@
  */
 public class Game {
     private int score;
+    private int counter = 0;
+    private int[] rolls = new int[21];
 
-    public void roll(int timesRolled, int pinsKnockedDown) {
-        for (int i = 0; i < timesRolled; i++) {
-            score += pinsKnockedDown;
-        }
+    public void roll(int pinsKnockedDown) {
+        rolls[counter++] = pinsKnockedDown;
     }
 
-    public int getScore() {
+    public int score() {
+        int score = 0;
+        int currentFrame = 0;
+        for (int i = 0; i < 10; i++) {
+            score += rolls[currentFrame] + rolls[currentFrame + 1];
+            currentFrame += 2;
+        }
         return score;
     }
 
+    public int getRoll() {
+        return counter;
+    }
 }
--- BowlingGameKata/src/test/java/GameTest.java
+++ BowlingGameKata/src/test/java/GameTest.java
@@ -1,3 +1,4 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -7,25 +8,44 @@ import static org.hamcrest.Matchers.is;
  * Created by DreamShine on 08/10/2016.
  */
 public class GameTest {
+    private Game game;
+
+    @Before
+    public void setUp() {
+        game = new Game();
+    }
+
     @Test
     public void TestGutterGameScoresZero() throws Exception {
-        Game game = new Game();
-        game.roll(1, 0);
-        assertThat(game.getScore(), is(0));
+        game.roll(0);
+        assertThat(game.score(), is(0));
     }
 
     @Test
     public void TestThrowTwentyGutterBallsScoresZero() throws Exception {
-        Game game = new Game();
-        game.roll(20, 0);
-        assertThat(game.getScore(), is(0));
+        game.roll(0);
+        assertThat(game.score(), is(0));
     }
 
     @Test
     public void TestThrowTwentyRollsOfOneScoresTwenty() throws Exception {
-        Game game = new Game();
-        game.roll(20, 1);
-        assertThat(game.getScore(), is(20));
+
+        for (int i = 0; i < 20; i++) {
+            game.roll(1);
+        }
+        assertThat(game.score(), is(20));
     }
 
+    @Test
+    public void TestThrowSpareThenFourScoresForteen() {
+        game.roll(4);
+        game.roll(6);
+        game.roll(4);
+        //--Roll the rest as zeros
+        for (int i = 0; i < 17; i++) {
+            game.roll(0);
+        }
+        assertThat(game.score(), is(14));
+    }
+    
 }
