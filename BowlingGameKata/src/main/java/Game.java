45ba1f7787883ec73e98bff65da1df7a65f8393a
DreamShine <wizard.jackalope@gmail.com>
2016-10-08 01:35:53 -0500

    First Test Passing, if gutterball then score 0 points.

--- BowlingGameKata/src/main/java/Game.java
+++ BowlingGameKata/src/main/java/Game.java
@@ -2,5 +2,13 @@
  * Created by DreamShine on 08/10/2016.
  */
 public class Game {
+    private int score;
 
+    public int roll(int pinsKnockedDown) {
+        return 0;
+    }
+
+    public Integer getScore() {
+        return 0;
+    }
 }
--- BowlingGameKata/src/test/java/GameTest.java
+++ BowlingGameKata/src/test/java/GameTest.java
@@ -1,6 +1,16 @@
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
 /**
  * Created by DreamShine on 08/10/2016.
  */
 public class GameTest {
-
+    @Test
+    public void TestGutterGameScoresZero() throws Exception {
+        Game game = new Game();
+        game.roll(0);
+        assertThat(game.getScore(), is(0));
+    }
 }
