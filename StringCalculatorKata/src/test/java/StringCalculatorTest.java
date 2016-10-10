5d659d34144ef518c51af00a56c047d613bcc43f
DreamShine <wizard.jackalope@gmail.com>
2016-10-09 19:38:24 -0500

    Refactored addition helper method - addStringNumbers() to clean up the tests. Wrote new failing test to check that a String of "1" returns 1

--- StringCalculatorKata/src/test/java/StringCalculatorTest.java
+++ StringCalculatorKata/src/test/java/StringCalculatorTest.java
@@ -10,16 +10,28 @@ import static org.junit.Assert.assertThat;
 public class StringCalculatorTest {
 
     private StringCalculator stringCalculator;
+    private int result;
 
     @Before
     public void setUp() throws Exception {
         stringCalculator = new StringCalculator();
+        result = 0;
     }
 
     @Test
     public void emptyStringReturnsZero() {
-        int result = stringCalculator.add("");
+        addStringNumbers("");
         assertThat(result, is(0));
+    }
+
+    private int addStringNumbers(String s) {
+        return stringCalculator.add(s);
+    }
+
+    @Test
+    public void testStringOfOneReturnsOne() {
+        addStringNumbers("1");
+        assertThat(result, is(1));
     }
 
 }
